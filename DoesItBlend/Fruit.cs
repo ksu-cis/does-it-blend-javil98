40cfa7e417ddaad0fc9b924d0a67071f5ba370ad
Jose Avila <javil@ksu.edu>
2019-09-20 14:29:24 -0500

    Changed

--- DoesItBlend/Fruit.cs
+++ DoesItBlend/Fruit.cs
@@ -4,10 +4,13 @@ using System.Text;
 
 namespace DoesItBlend
 {
+    //The differnce between the abstract and interface, is that abstract has a heirarchyl structure and is of nature, while a interface "does a".
+
     /// <summary>
     /// A base class representing fruit
     /// </summary>
-    public abstract class Fruit
+    public abstract class Fruit : IBlendable
     {
+        public abstract string Blend();
     }
 }
---
+++ DoesItBlend/IBlendable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DoesItBlend
+{
+    /// <summary>
+    /// A interface for items that are blendable.
+    /// </summary>
+    interface IBlendable
+    {
+        /// <summary>
+        /// Blends the object;
+        /// </summary>
+        /// <returns></returns>
+        string Blend();
+    }
+}
